0a0efd973c786dd9b35a9a7ddd2a015a5de40e95
Tony Sorensen <tony48853@gmail.com>
2021-05-23 21:00:05 -0700

    server set up

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const server = express();
 
 // DO YOUR MAGIC
+const carsRouter = require("./cars/cars-router");
+server.use(express.json());
 
-module.exports = server
+server.use("/api/cars", carsRouter);
+
+module.exports = server;
